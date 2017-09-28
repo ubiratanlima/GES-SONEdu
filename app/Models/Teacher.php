d09612e47c09769067f9457ca76265f3f76de2ab
ubiratanlima <ubiratan.lima@gmail.com>
2017-09-28 20:07:20 -0300

    continuando-2
    aula de polimorfismo.

---
+++ app/Models/Teacher.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GES\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    public function user(){
+        return $this->morphOne(User::class, 'userable');
+    }
+}
---
+++ app/Notifications/UserCreated.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace GES\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserCreated extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $appName = config('app.name');
+        return (new MailMessage)
+            ->subject("Sua conta no $appName foi criada")
+            ->greeting("Olá {$notifiable->name}, seja bem-vindo ao $appName!")
+            ->line("Seu número de matricula é {$notifiable->enrolment}.")
+            ->salutation("")
+//                    ->line('The introduction to the notification.')
+//                    ->action('Notification Action', url('/'))
+                    ->line('Obrigado por utilizar nossa APP');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
